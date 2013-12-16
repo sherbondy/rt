8cc75230201636106457e99d92e0431f23e87549
Ethan Sherbondy <sherbondye@gmail.com>
2013-12-16 10:56:50 -0500

    untested shapes and intersections

--- src/rt/core.clj
+++ src/rt/core.clj
@@ -1,6 +1,8 @@
 (ns rt.core
   (:require [clojure.core.typed :as typed
-             :refer [ann ann-many ann-record]]
+             :refer [ann ann-datatype ann-many ann-protocol ann-record
+                     def-alias def> defprotocol>
+                     letfn>]]
             [clojure.math.numeric-tower :as math]))
 
 """
@@ -85,13 +87,15 @@ Every subsection in Htrace could probably be its own namespaced module...
 (defn p+v [{:keys [x y z]} {a :x b :y c :z}]
   (Point3. (+ x a) (+ y b) (+ z c)))
 
+;; now I am feeling the pain, should really be using multimethods to define addition
+;; maybe I should make a macro for commutative operations?
+
 ;; does origin make more sense than base?
 (ann-record Ray [base :- Point3  direction :- Vector3])
 (defrecord Ray [base direction])
 
-;; now I am feeling the pain, should really be using multimethods...
-;; maybe I should make a macro for commutative operations?
-(ann position-at-time [Ray Number -> Point3])
+(def-alias Time Number)
+(ann position-at-time [Ray Time -> Point3])
 (defn position-at-time [{:keys [base direction]} time]
   (p+v base (k*v time direction)))
 
@@ -106,12 +110,15 @@ Every subsection in Htrace could probably be its own namespaced module...
          (* 0.5 (- negative-b root))]))))
 
 ;; time to bring in the core match?
-(ann xor [Bool Bool -> Bool])
+(ann xor [Boolean Boolean -> Boolean])
 (defn xor [a b]
   (condp = a
     true (not b)
     false b))
 
+(ann negate [Number -> Number])
+(defn negate [n]
+  (- 0 n))
 
 ;; oh, could core/typed do (runtime?) bounds enforcing on numbers?
 ;; that'd be incredible
@@ -120,14 +127,14 @@ Every subsection in Htrace could probably be its own namespaced module...
 
 (ann-many Color
           red green blue white
-          black midgrey nearlywhite)
-(def red         (Color. 1.0 0.0 0.0))
-(def green       (Color. 0.0 1.0 0.0))
-(def blue        (Color. 0.0 0.0 1.0))
-(def white       (Color. 1.0 1.0 1.0))
-(def black       (Color. 0.0 0.0 0.0))
-(def midgrey     (Color. 0.5 0.5 0.5))
-(def nearlywhite (Color. 0.8 0.8 0.8))
+          black mid-grey nearly-white)
+(def red          (Color. 1.0 0.0 0.0))
+(def green        (Color. 0.0 1.0 0.0))
+(def blue         (Color. 0.0 0.0 1.0))
+(def white        (Color. 1.0 1.0 1.0))
+(def black        (Color. 0.0 0.0 0.0))
+(def mid-grey     (Color. 0.5 0.5 0.5))
+(def nearly-white (Color. 0.8 0.8 0.8))
 
 (ann k*col [Number Color -> Color])
 (defn k*col [k {:keys [r g b]}]
@@ -154,8 +161,99 @@ Every subsection in Htrace could probably be its own namespaced module...
 
 ;; = combine_col
 ;; it would be amazing to simply define a binary operation,
-;; say that it is associative+commutative, and get an efficent
+;; specify that it is associative and commutative, and get an efficent
 ;; variadic version for free...
 (ann col* [Color Color -> Color])
 (defn col* [{:keys [r g b]} {r2 :r g2 :g b2 :b}]
   (Color. (* r r2) (* g g2) (* b b2)))
+
+
+;; procedural textures are functions of points that emit materials
+(ann-record Material [color :- Color, reflectivity :- Number, diffuseness :- Number])
+(defrecord Material [color reflectivity diffuseness])
+
+(ann-many [Point3 -> Material]
+          flat-red shiny-red semi-shiny-green shiny-white checked-matt)
+
+(defn flat-red [_] (Material. red 0.0 1.0))
+(defn shiny-red [_] (Material. red 0.3 0.9))
+(defn semi-shiny-green [_] (Material. green 0.5 0.7))
+(defn shiny-white [_] (Material. white 0.3 0.9))
+
+(def> matt-size :- Number 20.0)
+
+(ann matt-dim-even? [Number -> Boolean])
+(defn matt-dim-even? [dim]
+  (even? (int (/ dim matt-size))))
+
+(defn checked-matt [{:keys [x y z]}]
+  (let [x-even? (matt-dim-even? x)
+        y-even? (matt-dim-even? y)
+        z-even? (matt-dim-even? z)]
+    (if (xor (xor x-even? y-even?) z-even?)
+      (Material. white 0.0 1.0)
+      (Material. black 0.0 1.0))))
+
+
+;; SHAPES
+
+(ann-record Intersection [normal :- Vector3, point :- Point3, ray :- Ray, material :- Material])
+(defrecord Intersection [normal point ray material])
+
+(def> epsilon :- Number 0.001)
+
+(def-alias TimeIntersectVec (IPersistentVector Time Intersection))
+
+(ann-protocol Shape
+  intersect [Shape Ray -> TimeIntersectVec])
+(defprotocol> Shape
+  (intersect [shape ray]))
+
+(def-alias MaterialFn [Point3 -> Material])
+
+(ann-datatype Sphere [center :- Point3, radius :- Number, material-fn :- MaterialFn])
+(deftype Sphere [center radius material-fn]
+  Shape
+  (intersect [this {:keys [base direction] :as ray}]
+    (let [a (squared-mag direction)
+          b (* 2 (dot direction (- base center)))
+          c (- (squared-mag (- base center))
+               (* radius radius))
+          times (filter #(> % epsilon) (roots a b c))]
+      (letfn> [normal-at-time :- [Time -> Vector3]
+               (normal-at-time [t]
+                 (normalize (- (position-at-time t ray) center)))
+               intersection-at-time :- [Time -> Intersection]
+               (intersection-at-time [t]
+                 (let [pos (position-at-time ray t)]
+                  (Intersection. (normal-at-time t) pos ray (material-fn pos))))]
+             (map #([% (intersection-at-time %)]) times)))))
+
+;; Plane is defined by a normal (its a 2 sided plane though) and a distance.
+;; The plane coincident with y=5 and normal (0,0,1) has distance -5.
+(ann-datatype Plane [normal:- Vector3, distance :- Number, material-fn :- MaterialFn])
+(deftype Plane [normal distance material-fn]
+  Shape
+  (intersect [this {:keys [base direction] :as ray}]
+    (let [nnorm (normalize normal)
+          vd    (dot nnorm direction)
+          v0    (negate (+ (dot nnorm base) distance))]
+      (if (= vd 0.0)
+        []
+        (let [t (/ v0 vd)
+              hit-point (position-at-time ray t)]
+          (if (> t epsilon)
+            [t (Intersection.
+                (if (> vd 0) (neg normal) normal)
+                hit-point ray (material-fn hit-point))]
+            []))))))
+
+;; might this get an empty vector as the argument? In which case the result would be nil.
+(ann closest [(IPersistentVector TimeIntersectVec) -> (U Intersection nil)])
+;; I guess we do this trickery with reduce to support select-nearest when xs is empty
+(defn closest [xs]
+  (letfn> [select-nearest :- [TimeIntersectVec TimeIntersectVec -> TimeIntersectVec]
+           (select-nearest [[t1 i1] [t2 i2]]
+             (if (< t1 t2) [t1 i1] [t2 i2]))]
+    (second (reduce select-nearest (first xs) (rest xs)))))
+
